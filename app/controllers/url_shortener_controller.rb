0d5ac8c13eb4ec414aad5ee9a77f04c933e735f7
Andrew Tolopko <github@tolopko.com>
2017-04-16 23:31:45 -0400

    Communicate with conjurl API

--- app/controllers/url_shortener_controller.rb
+++ app/controllers/url_shortener_controller.rb
@@ -3,8 +3,19 @@ class UrlShortenerController < ApplicationController
   end
   
   def shorten
-    @long_url = params[:long_url]
-    @short_url = "http://shorter.com/ABCDEFG"
+    resp = RestClient.post("http://localhost:3001/api/short_urls", target_url: params[:long_url])
+    resp_data = JSON.parse(resp, symbolize_names: true)
+    Rails.logger.debug(resp_data)
+    @long_url = resp_data[:target_url]
+    @short_url = resp_data[:short_url] 
+    render 'index'
+  rescue RestClient::Exception => e
+    if e.http_code == 400
+      flash.now[:error] = "Error! #{JSON.parse(e.response)['error']}"
+    else
+      flash.now[:error] = "Sorry, something went wrong!"
+      Rails.logger.error(e)
+    end
     render 'index'
   end
 end
